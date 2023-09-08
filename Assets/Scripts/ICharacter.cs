4f98f060ad2222d4b72d33d4c3d7cfed81011955
kksoo0131 <a67636521@gmail.com>
2023-09-08 16:58:00 +0900

    ICharacter 생성

---
+++ Assets/Scripts/ICharacter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharacter
+{
+    protected void Attack();
+    protected void Attacked();
+
+}
