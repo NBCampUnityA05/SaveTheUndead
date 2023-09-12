5b541248a553c7debacca2ff9626a8daff33093f
Munch310 <ghkdlsi@naver.com>
2023-09-12 16:31:47 +0900

    [ADD & UPDATE] UIManager & MainScene - GameOverUI
    - UIMnager를 추가했습니다.
    
    따라서 버튼 함수를 수정했습니다.
    
    - MainScene - GameOverUI
    
    MainScene에서 GameOverUI를 구현했습니다.
    
    RetryBtn을 누르면 게임이 재시작 됩니다.

---
+++ Assets/Scripts/UI/MoveStartSceneBtn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStartSceneBtn : MonoBehaviour
+{
+    public void OnClickedMoveStartSceneBtn()
+    {
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.Select);
+        AudioManager.instance.PlayBgm(false);
+        UIManager.instance.LoadScene("StartScene");
+    }
+}
