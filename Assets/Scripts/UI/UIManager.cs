7bf6b172d9be9d8e5ee053bed0e5076daffff9bc
Munch310 <ghkdlsi@naver.com>
2023-09-12 20:02:42 +0900

    [UPDATE] UI BugFix
    - 게임을 재시작 했을 때, 애니메이션이 실행되지 않는 버그를 수정했습니다.
    
    - 게임을 재시작 했을 때, DontDestroyOnLoad가 중복으로 생성되는 버그를 수정했습니다.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,8 +7,11 @@ public class UIManager : MonoBehaviour
 
     void Awake()
     {
-        instance = this;
-        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     public void LoadScene(string sceneName)
