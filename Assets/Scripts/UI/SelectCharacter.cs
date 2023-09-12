3e2a37c1719979eb36046119f3e46f7b93f51c59
Munch310 <ghkdlsi@naver.com>
2023-09-12 16:35:13 +0900

    Revert "Revert "[ADD & UPDATE] UIManager & MainScene - GameOverUI""
    This reverts commit 22baa7774565e579a95ae14d12bbe96df9df53f2.

--- Assets/Scripts/UI/SelectCharacter.cs
+++ Assets/Scripts/UI/SelectCharacter.cs
@@ -7,10 +7,19 @@ using UnityEngine.UI;
 public class SelectCharacter : MonoBehaviour
 {
     public GameObject character;
+
     // CharacterIndex -> MainScene
     // AddListener 사용 예정. 추후 변경 가능
     public void OnclickedCharacter()
     {
-        SceneManager.LoadScene("MainScene");
+        if (character != null)
+        {
+            // character 객체가 null이 아닐 때만 아래 코드 실행
+            UIManager.instance.LoadScene("MainScene");
+        }
+        else
+        {
+            Debug.LogError("character 객체가 할당되지 않았습니다.");
+        }
     }
 }
